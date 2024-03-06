1baf5561ce4ffc58b00ddbd351cd390012c82ea0
xuanbao <bao.lgx@gmail.com>
2024-03-06 21:53:56 +0700

    fix

--- app/components/education/index.tsx
+++ app/components/education/index.tsx
@@ -68,7 +68,7 @@ const Detail = ({type, time, place, info}: InterfaceDetail) => {
             <motion.div
                 initial={{y: 50}}
                 whileInView={{y: 0}}
-                transtion={{duration: 0.5, type: 'spring'}}
+                transition={{duration: 0.5, type: 'spring'}}
             >
                 <h3 className='capitalize font-bold text-2xl sm:text-xl xs:text-lg'>
                     {type}
