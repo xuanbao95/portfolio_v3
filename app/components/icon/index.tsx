48359e0a27e5ac2ecc05af4c75fd9a6a94aa29b5
xuanbao <bao.lgx@gmail.com>
2024-03-06 22:04:06 +0700

    fix

--- app/components/icon/index.tsx
+++ app/components/icon/index.tsx
@@ -1,4 +1,4 @@
-const TwitterIcon = ({className, ...props}) => (
+const TwitterIcon = ({className, ...props}:{className: string}) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
         width="1.23em"
