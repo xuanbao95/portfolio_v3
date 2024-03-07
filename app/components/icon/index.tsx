5949fb2d18d7b4162f2fa1f1c4e6abab6f30709b
bao95 <bao.luugx@upos.vn>
2024-03-07 09:54:48 +0700

    change file jpg

--- app/components/icon/index.tsx
+++ app/components/icon/index.tsx
@@ -1,4 +1,4 @@
-const TwitterIcon = ({className, ...props}:{className: string}) => (
+const TwitterIcon = ({className, ...props}:{className: any}) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
         width="1.23em"
@@ -16,7 +16,7 @@ const TwitterIcon = ({className, ...props}:{className: string}) => (
 )
 export default TwitterIcon;
 
-export const GithubIcon = ({className, ...rest}:{className: string}) => (
+export const GithubIcon = ({className, ...rest}:{className: any}) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
         width="1em"
@@ -32,7 +32,7 @@ export const GithubIcon = ({className, ...rest}:{className: string}) => (
         />
     </svg>
 );
-export const LinkedInIcon = ({className, ...rest}:{className: string}) => (
+export const LinkedInIcon = ({className, ...rest}:{className: any}) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
         width="1em"
@@ -52,7 +52,7 @@ export const LinkedInIcon = ({className, ...rest}:{className: string}) => (
         </g>
     </svg>
 );
-export const FaceBookIcon = ({className, ...rest}:{className: string}) => (
+export const FaceBookIcon = ({className, ...rest}:{className: any}) => (
     <svg {...rest}
          className={`w-full h-auto ${className}`} xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="126.445 2.281 589 589" id="facebook">
         <circle cx="420.945" cy="296.781" r="294.5" fill="#3c5a9a"></circle>
@@ -60,7 +60,7 @@ export const FaceBookIcon = ({className, ...rest}:{className: string}) => (
               d="M516.704 92.677h-65.239c-38.715 0-81.777 16.283-81.777 72.402.189 19.554 0 38.281 0 59.357H324.9v71.271h46.174v205.177h84.847V294.353h56.002l5.067-70.117h-62.531s.14-31.191 0-40.249c0-22.177 23.076-20.907 24.464-20.907 10.981 0 32.332.032 37.813 0V92.677h-.032z"></path>
     </svg>
 )
-export const PinterestIcon = ({className, ...rest}:{className: string}) => (
+export const PinterestIcon = ({className, ...rest}:{className: any}) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
         width="1em"
@@ -77,7 +77,7 @@ export const PinterestIcon = ({className, ...rest}:{className: string}) => (
     </svg>
 );
 
-export const DribbbleIcon = ({className, ...rest}:{className: string}) => (
+export const DribbbleIcon = ({className, ...rest}:{className: any}) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
         width="1em"
@@ -98,7 +98,7 @@ export const DribbbleIcon = ({className, ...rest}:{className: string}) => (
     </svg>
 );
 
-export const SunIcon = ({className, ...rest}:{className: string}) => (
+export const SunIcon = ({className, ...rest}:{className: any}) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
         width="24"
@@ -277,7 +277,7 @@ export const SunIcon = ({className, ...rest}:{className: string}) => (
     </svg>
 );
 
-export const MoonIcon = ({className, ...rest}:{className: string}) => (
+export const MoonIcon = ({className, ...rest}:{className: any}) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
         width="24"
