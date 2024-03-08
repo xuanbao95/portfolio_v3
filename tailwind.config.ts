0a697ea8da77198f736d6d921b51d6f509942ef7
bao95 <bao.luugx@upos.vn>
2024-03-08 08:50:38 +0700

    fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,7 +67,6 @@ const config: Config = {
                 // => @media (max-width: 479px) { ... }
             },
 
-            backgroundColor: ["#121212", "rgba(131,58,180,1)", "rgba(253,29,29,1)", "rgba(252,176,69,1)", "rgba(131,58,180,1)", "#121212"],
         },
         screens: {
             "2xl": { max: "1535px" },
